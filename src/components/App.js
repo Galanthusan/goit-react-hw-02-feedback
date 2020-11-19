4168e44e38756349f1525bf7934ac8d3c3307f27
lesogorovalex <galanthusan@gmail.com>
2020-11-19 19:10:04 +0200

    Fixes

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,8 @@
-import React, { Component } from "react";
-import Section from "./Section/Section";
-import FeedbackOptions from "./FeedbackOptions/FeedbackOptions";
-import Statistics from "./Statistics/Statistics";
-import Notification from "./Notification/Notification";
+import React, { Component } from 'react';
+import Section from './Section/Section';
+import FeedbackOptions from './FeedbackOptions/FeedbackOptions';
+import Statistics from './Statistics/Statistics';
+import Notification from './Notification/Notification';
 
 class App extends Component {
   state = {
@@ -12,41 +12,37 @@ class App extends Component {
   };
 
   handleIncrement = (value) => {
-    console.log(value, "value");
     this.setState((prevState) => ({
       [value]: prevState[value] + 1,
     }));
-    this.countTotalFeedback();
-    this.countPositiveFeedbackPercentage();
   };
 
   countTotalFeedback = () => {
     return Object.values(this.state).reduce((acc, value) => acc + value, 0);
   };
 
-  countPositiveFeedbackPercentage = (total, positive) => {
-    return Math.round((positive * 100) / total);
+  countPositiveFeedbackPercentage = () => {
+    const { good } = this.state;
+    const total = this.countTotalFeedback();
+    return Math.round((good * 100) / total);
   };
 
   render() {
     const { good, neutral, bad } = this.state;
 
     const total = this.countTotalFeedback();
-    const positivePercantage = this.countPositiveFeedbackPercentage(
-      total,
-      good
-    );
+    const positivePercantage = this.countPositiveFeedbackPercentage();
 
     return (
       <>
-        <Section title="Please leave feedback">
+        <Section title='Please leave feedback'>
           <FeedbackOptions
             options={this.state}
             onLeaveFeedback={this.handleIncrement}
           />
         </Section>
-        {this.countTotalFeedback() > 0 ? (
-          <Section title="Statistics">
+        {total > 0 ? (
+          <Section title='Statistics'>
             <Statistics
               good={good}
               neutral={neutral}
@@ -56,7 +52,7 @@ class App extends Component {
             />
           </Section>
         ) : (
-          <Notification message="No feedback given" />
+          <Notification message='No feedback given' />
         )}
       </>
     );
